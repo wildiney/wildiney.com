2ee085afb05bf8bb05f6b245d38f07d185ad28bd
Wildiney Di Masi <wildiney@gmail.com>
2022-03-24 11:57:40 -0300

    refining code

---
+++ pages/api/linktree.tsx
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface ISocialMedia {
+  icon: string;
+  link: string;
+  site: string;
+}
+
+const socialMedia: ISocialMedia[] = [
+  {
+    icon: "AiOutlineBehance",
+    site: "Behance",
+    link: "https://www.behance.net/wildiney",
+  },
+  {
+    icon: "AiOutlineYoutube",
+    site: "YouTube",
+    link: "https://www.youtube.com/watch?v=5aU_i7rOnlA&list=PLEFS0XFJyys1QVGZ7WL23hCtWcLj4NV2y",
+  },
+  {
+    icon: "AiOutlineLinkedin",
+    site: "LinkedIn",
+    link: "https://linkedin.com/in/wildiney",
+  },
+  {
+    icon: "AiOutlineGithub",
+    site: "Github",
+    link: "https://github.com/wildiney/",
+  },
+
+  {
+    icon: "AiOutlineWhatsApp",
+    site: "WhatsApp",
+    link: "https://wa.me/5511981591082",
+  },
+  {
+    icon: "AiOutlineMail",
+    site: "E-mail",
+    link: "mailto:wildiney@gmail.com",
+  },
+];
+async function handler(_: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json(socialMedia);
+}
+
+export default handler;
