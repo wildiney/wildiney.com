ccf04618e40b1e05b15d6eadf633cae7c49cb4db
Wildiney Di Masi <wildiney@gmail.com>
2022-03-24 14:37:41 -0300

    add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as gtag from "../libs/gtag";
+import Analytics from "../Components/GoogleAnalytics";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
