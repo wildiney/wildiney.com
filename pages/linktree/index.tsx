6b962da5390586d38cea94e4b38793241035f206
Wildiney Di Masi <wildiney@gmail.com>
2022-03-24 13:59:25 -0300

    add page linktree

---
+++ pages/linktree/index.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import LinkTree from "../../Components/LinkTree";
+
+export default function Home({ data }) {
+  return (
+    <>
+      <Head>
+        <title>Wildiney Fernando Pimentel - LinkTree</title>
+      </Head>
+      <LinkTree data={data} />
+    </>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch(`${process.env.URL}/api/linktree`);
+  const data = await response.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
