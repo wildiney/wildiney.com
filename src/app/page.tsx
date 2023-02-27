124c789bf32dcabf9386aeaf44d6072d0747eade
Wildiney Di Masi <wildiney@gmail.com>
2023-02-27 12:01:33 -0300

    add variable url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import LinkTree from "./components/LinkTree";
 
 export default async function Page() {
 
-  const response = await fetch(`https://www.wildiney.com/api/linktree`);
+  const response = await fetch(`${process.env.URL}/api/linktree`);
   const data = await response.json();
 
   return (
