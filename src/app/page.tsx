91c86f831089ebd718a6e85cca1b89459d5122c9
Wildiney Di Masi <wildiney@gmail.com>
2023-02-27 11:39:26 -0300

    add home

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import LinkTree from "./components/LinkTree";
+
+export default async function Page() {
+
+  const response = await fetch(`${process.env.URL}/api/linktree`);
+  const data = await response.json();
+
+  return (
+    <>
+      <LinkTree data={data} />
+    </>
+  );
+}
