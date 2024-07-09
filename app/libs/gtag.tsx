3e61f1ea32f99b7129ceab1260a186454e8f0e90
Wildiney Di Masi <wildiney@gmail.com>
2024-07-09 16:58:30 -0300

    refactor: update google analytics code

--- app/libs/gtag.tsx
+++ app/libs/gtag.tsx
@@ -2,19 +2,25 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 declare global {
   interface Window {
-    gtag: any;
+    gtag: (...args: any[]) => void;
   }
 }
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+
+interface EventParams {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+}
+
 export const pageview = (url: string) => {
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }: any) => {
-  window.gtag("event", action, {
+export const event = ({ action, category, label, value }: EventParams) => {
+  window.gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
