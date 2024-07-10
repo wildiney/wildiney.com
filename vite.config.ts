e9240c7e427f06b03372d3fcc54509230c20fc2e
Wildiney Di Masi <wildiney@gmail.com>
2024-07-10 13:13:50 -0300

    build: add gh-pages and remove vite-plugin-gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +12,5 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    ghPages()
   ],
 })
