752f317170cb3cef1a815e51e843cf1fe91eee06
Wildiney Di Masi <wildiney@gmail.com>
2024-07-11 13:18:33 -0300

    refactor: add analytics to buttons

--- src/components/Linktree/LinkTree.tsx
+++ src/components/Linktree/LinkTree.tsx
@@ -3,9 +3,9 @@ import { ISocialMedia } from "@/data/data";
 
 export default function LinkTree ({ data }: { data: ISocialMedia[] }) {
   return (
-    <div className="flex flex-col w-full p-4 mx-auto md:w-6/12 xl:w-4/12">
-      <h1 className="m-0 text-3xl font-thin text-center text-white md:text-3xl lg:text-4xl font-josefin">Wildiney Fernando Pimentel</h1>
-      <h2 className="m-2 mb-4 text-2xl text-center font-extralight text-secondary">Designer & Developer</h2>
+    <div className="flex flex-col items-center w-full max-w-xl p-4 mx-auto min-w-80">
+      <h1 className="m-0 text-3xl font-light text-center text-white md:text-3xl lg:text-4xl font-josefin">Wildiney Fernando Pimentel</h1>
+      <h2 className="m-2 mb-8 text-2xl font-light text-center text-secondary">Designer & Developer</h2>
 
       {data &&
         data.map((button, index) => {
--- src/components/PortfolioButton/PortfolioButton.tsx
+++ src/components/PortfolioButton/PortfolioButton.tsx
@@ -1,9 +1,17 @@
 import styles from "./ButtonPortfolio.module.css";
+import ReactGA from 'react-ga4';
 
 export function ButtonPortfolio ({ icon, site, link }: { icon: React.ReactNode, site: string, link: string }) {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault
+    ReactGA.event({ category: "link", action: "click", label: site });
+    setTimeout(() => {
+      window.open(link, '_blank');
+    }, 100);
+  }
   return (
-    <div className="relative h-[80px]">
-      <a className={styles.btn} target="_blank" rel="noreferrer" href={link}>
+    <div className="relative h-[80px] w-full max-w-xl min-w-80 p-4">
+      <a className={styles.btn} target="_blank" rel="noreferrer" onClick={handleClick}>
         <span className={styles.icon}>{icon}</span>
         <span className={styles.text}>{site}</span>
       </a>
