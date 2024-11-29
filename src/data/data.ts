924da6c07f77fc33a771bea24e111d27e83ebae0
Wildiney Di Masi <wildiney@gmail.com>
2024-11-29 17:17:45 -0300

    change link order

--- src/data/data.ts
+++ src/data/data.ts
@@ -9,22 +9,16 @@ export type ISocialMedia = {
 
 export const socialMedia: ISocialMedia[] = [
   {
-    icon: reacticons.AiOutlineBehance,
-    site: 'Behance',
-    link: 'https://www.behance.net/wildiney',
-    ariaLabel: "See my portfolio at Behance"
-  },
-  {
-    icon: reacticons.AiOutlineYoutube,
-    site: 'YouTube',
-    link: 'https://www.youtube.com/watch?v=5aU_i7rOnlA&list=PLEFS0XFJyys1QVGZ7WL23hCtWcLj4NV2y',
-    ariaLabel: "See this playlist with my work on Youtube.com"
-  },
-  {
     icon: reacticons.AiOutlineLinkedin,
     site: 'LinkedIn',
     link: 'https://linkedin.com/in/wildiney',
     ariaLabel: "See my resumé on LinkedIn"
+  },
+  {
+    icon: reacticons.AiOutlineBehance,
+    site: 'Behance',
+    link: 'https://www.behance.net/wildiney',
+    ariaLabel: "See my portfolio at Behance"
   },
   {
     icon: reacticons.AiOutlineGithub,
@@ -32,7 +26,12 @@ export const socialMedia: ISocialMedia[] = [
     link: 'https://github.com/wildiney/',
     ariaLabel: "See some of my coding projects on Github"
   },
-
+  {
+    icon: reacticons.AiOutlineYoutube,
+    site: 'YouTube',
+    link: 'https://www.youtube.com/watch?v=5aU_i7rOnlA&list=PLEFS0XFJyys1QVGZ7WL23hCtWcLj4NV2y',
+    ariaLabel: "See this playlist with my work on Youtube.com"
+  },
   {
     icon: reacticons.AiOutlineWhatsApp,
     site: 'WhatsApp',
