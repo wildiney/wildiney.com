5f5964908ebb3aed8931c407ac5660b0deb10bc6
wildiney <wildiney@gmail.com>
2025-05-15 15:32:23 -0300

    improve analytics info

--- src/components/PortfolioButton/PortfolioButton.tsx
+++ src/components/PortfolioButton/PortfolioButton.tsx
@@ -4,7 +4,15 @@ import ReactGA from 'react-ga4';
 export function ButtonPortfolio ({ icon, site, link, ariaLabel }: { icon: React.ReactNode, site: string, link: string, ariaLabel?: string }) {
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault()
-    ReactGA.event({ category: "link", action: "click", label: site });
+    ReactGA.gtag('event', 'click', {
+      event_category: 'link',
+      event_label: site,
+      timestamp: new Date().toISOString(),
+      position_x: e.clientX,
+      position_y: e.clientY,
+      platform: window.innerWidth < 768 ? "mobile" : "desktop",
+      referrer: document.referrer
+    });
     setTimeout(() => {
       window.open(link, '_blank');
     }, 100);
