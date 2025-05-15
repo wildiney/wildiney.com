c9312fe06a937934f03ea2c6cc2ded8785f28482
Wildiney Di Masi <wildiney@gmail.com>
2025-05-15 13:02:08 -0300

    change last name

--- src/components/Linktree/LinkTree.tsx
+++ src/components/Linktree/LinkTree.tsx
@@ -7,7 +7,7 @@ export default function LinkTree ({ data }: { data: ISocialMedia[] }) {
       <a href="#main" className="sr-only">Skip to main content</a>
       <div className="flex flex-col items-center w-full h-screen max-w-xl p-4 mx-auto min-w-80 md:justify-center">
         <div className="p-8 border-2 rounded-lg shadow-lg bg-white/90 border-secondary">
-          <h1 className="m-0 mt-2 text-3xl font-light text-center text-secondary md:text-3xl lg:text-4xl font-josefin">Wildiney Fernando Pimentel</h1>
+          <h1 className="m-0 mt-2 text-3xl font-light text-center text-secondary md:text-3xl lg:text-4xl font-josefin">Wildiney Fernando Di Masi</h1>
           <h2 className="m-2 mb-8 text-2xl font-light text-center text-secondary">Product Designer | UI/UX Specialist</h2>
           <main className="w-full" id="main">
             <ul className="w-full p-0 m-0 mb-0">
