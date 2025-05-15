63dc2a69f77d50c8c8d6ceca1f8da418aba5f811
Wildiney Di Masi <wildiney@gmail.com>
2025-05-15 11:04:16 -0300

    update layout

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -12,7 +12,7 @@ export default function Background ({ children }: { children: ReactNode }) {
         <source src={bgMovie} media='(min-width: 1281px)' type="video/mp4" />
         Seu navegador não suporta vídeos.
       </video>
-      <div className="fixed top-0 left-0 w-full h-full bg-black/70" />
+      <div className="fixed top-0 left-0 w-full h-full bg-black/50" />
       <div className="relative z-10 flex flex-col items-center h-full overflow-y-auto text-white">
         {children}
       </div>
