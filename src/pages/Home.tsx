4e9c169ae8f0710a619fdb2b57afb3902ecf1591
Wildiney Di Masi <wildiney@gmail.com>
2025-07-09 18:34:02 -0300

    refactor: content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,12 @@
-import { useEffect } from 'react';
 import { socialMedia } from '../data/data';
-
-import ReactGA from 'react-ga4';
+import { usePageTracking } from '@/hooks/usePageTracking';
 
 import Background from '@/components/Background/Background';
 import LinkTree from '@/components/Linktree/LinkTree';
 
 function Home () {
-  useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: `${window.location.pathname} + ${window.location.search}`, title: "LinkTree" });
-  }, []);
+  usePageTracking("LinkTree")
+
   return (
     <Background>
       <LinkTree data={socialMedia} />
