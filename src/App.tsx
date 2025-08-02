6ed8fae2b61db234967b38a8d988d87fea056607
Wildiney Di Masi <wildiney@gmail.com>
2025-08-02 17:56:46 -0300

    add portfolio items

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,19 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import LinkTree from '@/pages/LinkTree';
 import ScrollToAnchor from './ScrollToAnchor';
+import BolsaFamilia from './pages/sections/portfolio/bolsa-familia';
+import EcommerceSanrio from './pages/sections/portfolio/sanrio';
+import ScrollToTop from '@/components/ScrollToTop';
 
 function App () {
   return (
     <HashRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/linktree" element={<LinkTree />} />
+        <Route path="/portfolio/bolsa-familia" element={<BolsaFamilia />} />
+        <Route path="/portfolio/sanrio/ecommerce" element={<EcommerceSanrio />} />
       </Routes>
       <ScrollToAnchor />
     </HashRouter>
