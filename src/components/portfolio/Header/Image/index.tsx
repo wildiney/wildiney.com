800ab5b63e396f14dae9960683bc7966949657d3
Wildiney Di Masi <wildiney@gmail.com>
2025-08-02 17:53:40 -0300

    convert portfolio image header to component

---
+++ src/components/portfolio/Header/Image/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function Image ({ img }: { img: string }) {
+  const [size, setSize] = useState(true)
+
+  return (
+    <div className={`w-full h-96 overflow-hidden mt-17 bg-center bg-gray-200`}>
+      <img src={img}
+        onClick={() => { setSize(!size) }}
+        className={`block mx-auto object-cover h-full  ${size ? " h-full w-full object-cover" : "md:h-full md:object-center w-5xl"} transition-all`} />
+    </div>
+  )
+}
+
+export default Image
