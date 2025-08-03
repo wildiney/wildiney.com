878acf6ef03fe85896011a2fc74c5175b75f39b5
Wildiney Di Masi <wildiney@gmail.com>
2025-08-03 16:49:22 -0300

    improve header image

--- src/components/portfolio/Header/Image/index.tsx
+++ src/components/portfolio/Header/Image/index.tsx
@@ -7,7 +7,7 @@ function Image ({ img }: { img: string }) {
     <div className={`w-full h-[32rem] overflow-hidden mt-17 bg-center bg-gray-200`}>
       <img src={img}
         onClick={() => { setSize(!size) }}
-        className={`block mx-auto object-cover h-full  ${size ? " h-full w-full object-cover" : "md:h-full md:object-center w-5xl"} transition-all`} />
+        className={`block mx-auto object-cover h-full  ${size ? " h-full w-full object-cover" : "md:h-full md:object-center w-5xl"} transition-all lg:hover:opacity-50`} />
     </div>
   )
 }
