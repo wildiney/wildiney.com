37229232feb2369c43227408bdbeae904cf63ee5
Wildiney Di Masi <wildiney@gmail.com>
2025-08-06 16:46:08 -0300

    fix: code structure

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import './i18n';
 import ReactGA from 'react-ga4';
 import './index.css'
 
-const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID
+const TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID;
 if (TRACKING_ID) {
-  ReactGA.initialize(TRACKING_ID)
+  ReactGA.initialize(TRACKING_ID);
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
