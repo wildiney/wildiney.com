e78cfb9bf7ce9ae567607620e677e3c447a5e3d4
Wildiney Di Masi <wildiney@gmail.com>
2025-08-06 18:14:28 -0300

    add eslint-plugin-react, eslint-plugin-a11y and configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,64 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    // Global ignores
+    ignores: ['dist/'],
+  },
+  // Base configurations
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  // Configuration for React files (TSX)
+  {
+    files: ['src/**/*.{ts,tsx}'],
     plugins: {
+      react,
       'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
       'react-refresh': reactRefresh,
     },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-)
+
+  // Configuration for Node.js files
+  {
+    files: ['vite.config.ts', 'eslint.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  }
+);
