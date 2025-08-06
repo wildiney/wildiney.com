2786f1ebe1bc1abdb6e0b29ed0e158a0417a575f
Wildiney Di Masi <wildiney@gmail.com>
2025-08-06 18:09:23 -0300

    fix: use html semantic for action

--- src/components/portfolio/Header/Image/index.tsx
+++ src/components/portfolio/Header/Image/index.tsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 
-function Image ({ img }: { img: string }) {
+function Image ({ img, alt }: { img: string, alt: string }) {
   const [size, setSize] = useState(true)
 
   return (
-    <div className={`w-full h-72 sm:h-96 md:h-[24rem] lg:h-[30rem] overflow-hidden mt-16 lg:mt-17 bg-center bg-gray-200`}>
-      <img src={img}
-        onClick={() => { setSize(!size) }}
-        className={`block mx-auto object-cover h-full  ${size ? " h-full w-full object-cover" : "md:h-full md:object-center w-[62rem]"} transition-all lg:hover:opacity-50`} />
-    </div>
+    <button className={`w-full h-72 sm:h-96 md:h-[24rem] lg:h-[30rem] overflow-hidden mt-16 lg:mt-17 bg-center bg-gray-200`} onClick={() => { setSize(!size) }}>
+      <img src={img} alt={alt} className={`block mx-auto object-cover h-full  ${size ? " h-full w-full object-cover" : "md:h-full md:object-center w-[62rem]"} transition-all lg:hover:opacity-50`} />
+    </button>
   )
 }
 
