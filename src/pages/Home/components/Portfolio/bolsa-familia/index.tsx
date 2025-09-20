4db7e07604fb3963fe205cc04733517ce163ccd5
Wildiney Di Masi <wildiney@gmail.com>
2025-09-19 22:09:31 -0300

    add gallery of deliverables

--- src/pages/Home/components/Portfolio/bolsa-familia/index.tsx
+++ src/pages/Home/components/Portfolio/bolsa-familia/index.tsx
@@ -1,9 +1,21 @@
-import ImgBolsaFamilia from '@/assets/portfolio/bolsa-familia/bolsa-familia.webp';
 import Header from "@/components/Header";
+import Modal from "@/components/Modal";
 import Image from "@/components/portfolio/Header/Image";
 import { renderLines } from '@/libs/renderLines';
-import { useTranslation } from 'react-i18next';
 import Contact from "@/pages/Home/components/Contact";
+import { useTranslation } from 'react-i18next';
+
+import ImgBolsaFamilia from '@/assets/portfolio/bolsa-familia/bolsa-familia.webp';
+import ImgDSColors from '@/assets/portfolio/bolsa-familia/ds-colors.webp';
+import ImgDSColorsSM from '@/assets/portfolio/bolsa-familia/ds-colors-sm.webp';
+import ImgDSTypography from '@/assets/portfolio/bolsa-familia/ds-typography.webp';
+import ImgDSTypographySM from '@/assets/portfolio/bolsa-familia/ds-typography-sm.webp';
+import ImgDSComponents from '@/assets/portfolio/bolsa-familia/ds-components.webp';
+import ImgDSComponentsSM from '@/assets/portfolio/bolsa-familia/ds-components-sm.webp';
+import ImgDSUserFlows from '@/assets/portfolio/bolsa-familia/ds-user-flows.webp';
+import ImgDSUserFlowsSM from '@/assets/portfolio/bolsa-familia/ds-user-flows-sm.webp';
+import ImgDSCharts from '@/assets/portfolio/bolsa-familia/ds-charts.webp';
+import ImgDSChartsSM from '@/assets/portfolio/bolsa-familia/ds-charts-sm.webp';
 
 function BolsaFamilia () {
   const { t } = useTranslation()
@@ -38,6 +50,16 @@ function BolsaFamilia () {
 
             <h2 className='text-3xl text-primary mt-8 mb-4'>{t('bolsafamilia.titleResultsAndImpacts')}</h2>
             <p className="leading-relaxed text-gray-700">{renderLines(t('bolsafamilia.contextResultsAndImpacts'))}</p>
+
+            <h2 className='text-3xl text-primary mt-8 mb-4'>{t('bolsafamilia.titleGallery')}</h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              <Modal thumbnailSrc={ImgDSColorsSM} largeSrc={ImgDSColors} alt="Design System - Colour Pallete" />
+              <Modal thumbnailSrc={ImgDSTypographySM} largeSrc={ImgDSTypography} alt="Design System - Typography" />
+              <Modal thumbnailSrc={ImgDSComponentsSM} largeSrc={ImgDSComponents} alt="Design System - Components" />
+              <Modal thumbnailSrc={ImgDSUserFlowsSM} largeSrc={ImgDSUserFlows} alt="Design System - User Flows" />
+              <Modal thumbnailSrc={ImgDSChartsSM} largeSrc={ImgDSCharts} alt="Research" />
+            </div>
 
 
           </div>
