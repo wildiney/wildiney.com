eda1db53d887735154b81f8029442d03641375d5
Wildiney Di Masi <wildiney@gmail.com>
2025-10-04 18:33:24 -0300

    feat: enhance page tracking to include hash in URL for analytics

--- src/hooks/usePageTracking.ts
+++ src/hooks/usePageTracking.ts
@@ -1,13 +1,17 @@
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 
 export const usePageTracking = () => {
+  const location = useLocation();
+
   useEffect(() => {
     const host = window.location.hostname;
     const isLocalhost = host === 'localhost' || host === '127.0.0.1' || host === '';
 
     if (process.env.NODE_ENV === 'production' && !isLocalhost) {
-      ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
+      const pagePath = location.pathname + location.search + location.hash.replace('#', '');
+      ReactGA.send({ hitType: 'pageview', page: pagePath });
     }
-  }, []);
+  }, [location]);
 };
